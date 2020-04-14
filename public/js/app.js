ee53889e497bec5d71a902d3e077f71e5f3644a7
Karikari1234 <42123922+Karikari1234@users.noreply.github.com>
2020-04-14 12:17:47 +0600

    First Commit

---
+++ public/js/app.js
@@ -0,0 +1,23 @@
+console.log('Client side javascript on')
+
+
+
+
+const form = document.getElementById('form')
+const namepara = document.getElementById('m1')
+
+
+
+if (form) form.addEventListener('submit', (e) => {
+    e.preventDefault()
+
+    const input = document.getElementById('input')
+    const query = input.value
+
+    if (query) fetch('http://localhost:3000/about?name=' + query).then((response) => {
+        response.json().then((data) => {
+            namepara.textContent = `Your name is ${data.name} and your age is ${data.age}`
+        })
+    })
+    else console.log('No input given')
+})
---
+++ src/app.js
@@ -0,0 +1,44 @@
+const express = require('express')
+const path = require('path')
+
+
+const app = express()
+// console.log(__filename)
+const publicDirectoryPath = path.join(__dirname, '../public')
+// console.log(publicDirectoryPath)
+
+app.set('view engine', 'hbs')
+
+app.get('', (req, res) => res.render('index', {
+    title: "Hello World",
+    name: "Naafiz Rahman"
+}))
+
+app.use(express.static(publicDirectoryPath))
+
+app.get('', (req, res) => res.send({
+    name: {
+        firstname: 'Karim',
+        lastname: 'Khan'
+    },
+    age: 27
+}))
+
+
+
+app.get('/about', (req, res) => {
+    if (req.query.name)
+        res.send({
+            name: req.query.name,
+            age: req.query.name.length * 5
+        })
+    else
+        res.send('No')
+})
+
+app.get('/help', (req, res) => res.send('Help'))
+
+app.get('/weather', (req, res) => res.send('Weather'))
+
+
+app.listen(3000, () => console.log('Server up and running'))
