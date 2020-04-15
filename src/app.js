4ae806f5092cffca38af763172e3a28af5608c3e
Karikari1234 <42123922+Karikari1234@users.noreply.github.com>
2020-04-15 22:06:15 +0600

    Client location testing

--- src/app.js
+++ src/app.js
@@ -1,7 +1,18 @@
 const express = require('express')
 const path = require('path')
 const request = require('request')
+var ip = require('ipaddr.js')
 
+function cleanupAddress(str) {
+    // if it's a valid ipv6 address, and if its a mapped ipv4 address,
+    // then clean it up. otherwise return the original string.
+    if (ip.IPv6.isValid(str)) {
+        var addr = ip.IPv6.parse(str);
+        if (addr.isIPv4MappedAddress())
+            return addr.toIPv4Address().toString();
+    }
+    return str
+}
 
 
 
@@ -16,22 +27,26 @@ const port = process.env.PORT || 3000
 const url = 'https://api.mapbox.com/geocoding/v5/mapbox.places/Dhaka.json?access_token=pk.eyJ1IjoibmFhZml6NDciLCJhIjoiY2s5MHkwb2tjMDZ4czNxdGozbHV4dmFwbyJ9.64AtTt7EFUh1IAaUw_A4GQ'
 
 var geolocationurl = 'https://api.snoopi.io/'
-request({
-    url: 'https://api.ipgeolocation.io/getip',
-    json: true
-}, (error, response) => {
-    if (response.body.ip) {
-        geolocationurl = geolocationurl + response.body.ip
-    } else
-        geolocationurl = ''
-})
+
+// request({
+//     url: 'https://api.ipgeolocation.io/getip',
+//     json: true
+// }, (error, response) => {
+//     if (response.body.ip) {
+//         geolocationurl = geolocationurl + response.body.ip
+//     } else
+//         geolocationurl = ''
+// })
 
 app.set('view engine', 'hbs')
 
-app.get('', (req, res) => res.render('index', {
-    title: "Hello World",
-    name: "Naafiz Rahman"
-}))
+app.get('', (req, res) => {
+    geolocationurl = geolocationurl + cleanupAddress(req.connection.remoteAddress)
+    res.render('index', {
+        title: "Hello World",
+        name: "Naafiz Rahman"
+    })
+})
 
 app.use(express.static(publicDirectoryPath))
 
@@ -76,8 +91,8 @@ app.get('/weather', (req, res) => {
             url: geolocationurl,
             json: true
         }, (error, response) => {
-            console.log(response)
-            if (response) {
+            //console.log(response)
+            if (response.body) {
                 res.send({
                     state: response.body.State,
                     city: response.body.City,
