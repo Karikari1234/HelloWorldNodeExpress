6f0c3ca9bc133b207c9b9e61cf474efcd098d868
Karikari1234 <42123922+Karikari1234@users.noreply.github.com>
2020-05-01 17:43:24 +0600

    more refactoring

--- src/app.js
+++ src/app.js
@@ -1,95 +1,94 @@
-const express = require('express')
-const path = require('path')
-const request = require('request')
-const ip = require('ipaddr.js')
-
+const express = require("express");
+const path = require("path");
+const request = require("request");
+const ip = require("ipaddr.js");
 
 function cleanupAddress(str) {
-    if (ip.IPv6.isValid(str)) {
-        var addr = ip.IPv6.parse(str);
-        if (addr.isIPv4MappedAddress())
-            return addr.toIPv4Address().toString();
-    }
-    return str
+  if (ip.IPv6.isValid(str)) {
+    var addr = ip.IPv6.parse(str);
+    if (addr.isIPv4MappedAddress()) return addr.toIPv4Address().toString();
+  }
+  return str;
 }
 
+const app = express();
 
+const publicDirectoryPath = path.join(__dirname, "../public");
 
-const app = express()
+const port = process.env.PORT || 3000;
 
+const url =
+  "https://api.mapbox.com/geocoding/v5/mapbox.places/Dhaka.json?access_token=pk.eyJ1IjoibmFhZml6NDciLCJhIjoiY2s5MHkwb2tjMDZ4czNxdGozbHV4dmFwbyJ9.64AtTt7EFUh1IAaUw_A4GQ";
 
-const publicDirectoryPath = path.join(__dirname, '../public')
+var geolocationurl = "https://api.snoopi.io/";
 
-const port = process.env.PORT || 3000
+app.set("view engine", "hbs");
 
+app.disable("trust proxy");
 
+app.get("", (req, res) => {
+  var ip = req.connection.remoteAddress;
+  console.log(ip);
+  geolocationurl = geolocationurl + cleanupAddress(ip);
+  res.render("index", {
+    title: "Hello World",
+    name: "Naafiz Rahman",
+  });
+});
 
-const url = 'https://api.mapbox.com/geocoding/v5/mapbox.places/Dhaka.json?access_token=pk.eyJ1IjoibmFhZml6NDciLCJhIjoiY2s5MHkwb2tjMDZ4czNxdGozbHV4dmFwbyJ9.64AtTt7EFUh1IAaUw_A4GQ'
+app.use(express.static(publicDirectoryPath));
 
-var geolocationurl = 'https://api.snoopi.io/'
+app.get("/about", (req, res) => {
+  if (req.query.name)
+    res.send({
+      name: req.query.name,
+      age: parseInt((req.query.name.charCodeAt(0) * 5) / 10),
+    });
+  else res.send("No");
+});
 
-
-app.set('view engine', 'hbs')
-
-app.disable('trust proxy')
-
-app.get('', (req, res) => {
-    var ip = req.connection.remoteAddress;
-    console.log(ip);
-    geolocationurl = geolocationurl + cleanupAddress(ip)
-    res.render('index', {
-        title: "Hello World",
-        name: "Naafiz Rahman"
-    })
-})
-
-app.use(express.static(publicDirectoryPath))
-
-app.get('/about', (req, res) => {
-    if (req.query.name)
+app.get("/location", (req, res) => {
+  request(
+    {
+      url: url,
+      json: true,
+    },
+    (error, response) => {
+      if (response.body.features)
         res.send({
-            name: req.query.name,
-            age: parseInt(req.query.name.charCodeAt(0) * 5 / 10)
-        })
-    else
-        res.send('No')
-})
+          message: response.body.features[0].place_name,
+        });
+      else
+        res.send({
+          message: "!",
+        });
+    }
+  );
+});
 
-app.get('/location', (req, res) => {
-    request({
-        url: url,
-        json: true
-    }, (error, response) => {
-        if (response.body.features) res.send({
-            message: response.body.features[0].place_name
-        })
-        else res.send({
-            message: '!'
-        })
-    })
+app.get("/weather", (req, res) => {
+  if (geolocationurl != "")
+    request(
+      {
+        url: geolocationurl,
+        json: true,
+      },
+      (error, response) => {
+        if (error) {
+          res.status(404).send();
+        }
+        if (response.body) {
+          res.send({
+            state: response.body.State,
+            city: response.body.City,
+            country: response.body.CountryName,
+          });
+        } else
+          res.send({
+            ip: geolocationurl,
+          });
+      }
+    );
+});
 
-})
-
-app.get('/weather', (req, res) => {
-    if (geolocationurl != '')
-        request({
-            url: geolocationurl,
-            json: true
-        }, (error, response) => {
-            if (error) {
-                res.status(404).send()
-            }
-            if (response.body) {
-                res.send({
-                    state: response.body.State,
-                    city: response.body.City,
-                    country: response.body.CountryName
-                })
-            } else res.send({
-                ip: geolocationurl
-            })
-        })
-})
-
-
-app.listen(port, () => console.log('Server up and running'))
+app.listen(port, () => console.log("Server up and running"));
